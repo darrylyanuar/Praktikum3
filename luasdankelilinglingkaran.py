8745c0a218ecec4a4c72f0326fda450476f4a9fb
darryl <darryl>
2022-11-03 06:58:59 +0700

    file lingkaran

---
+++ luasdankelilinglingkaran.py
@@ -0,0 +1,11 @@
+print("Program Python Hitung Luas dan Keliling Lingkaran")
+print("_________________________________________________\n")
+
+r = input("Masukkan jari-jari lingkaran :")
+pi = 3.14
+l = pi * int(r) * int(r)
+k = 2 * pi * int(r)
+print("\n_______________________________________________")
+print("Luas lingkaran:", l)
+print("Kelilinglingkaran:",k)
+
